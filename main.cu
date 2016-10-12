19121fed1cd28705cbe847de94c74cf36c34e073
alfred316 <ashaker@kent.edu>
2016-10-12 12:07:42 -0400

    added the barebones main file

---
+++ main.cu
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define DIM 512
+
+struct drone {
+	int x;
+	int y;
+	int dx;
+	int dy;
+}
+
+__global__ void flight(struct drone)
+{
+
+
+}
+
+
+int main (void)
+{
+
+
+}
