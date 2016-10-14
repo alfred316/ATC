3abfd141e399b4393fc3044293a7499e130ba80b
alfred316 <ashaker@kent.edu>
2016-10-14 19:17:09 -0400

    added some calculation code

--- main.cu
+++ main.cu
@@ -5,21 +5,42 @@
 #define DIM 512
 
 struct drone {
-	int x;
-	int y;
-	int dx;
-	int dy;
+	float x;
+	float y;
+	float dx;
+	float dy;
 }
 
-__global__ void flight(struct drone)
+__global__ void flight(struct drone, int n)
 {
+	drone.x = rand() % 129;
+	drone.y = rand() % 129;
+	
+	//drone.dx = ((rand() % 600 + 30)/3600) * 0.5;	
 
+	int i = 0;	
+	while (i < n)
+	{
+		drone.dx = ((rand() % 600 + 30)/3600) * 0.5;
+		drone.dy = ((rand() % 600 + 30)/3600) * 0.5;
+		drone.x += drone.dx;
+		drone.y += drone.dy;
+		if(drone.x >= 128)
+		{
+			drone.x = drone.x * (-1);
+		}		
+		if(drone.y >= 128)
+		{
+			drone.y = drone.y * (-1);
+		}
+		printf("", drone.x, drone.y);
 
+	}
 }
 
 
 int main (void)
 {
-
+	srand(time(NULL));
 
 }
