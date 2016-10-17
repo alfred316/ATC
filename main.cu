b15e5465e4bb344fced1b144014e4ca4e4cdb170
alfred316 <ashaker@kent.edu>
2016-10-17 13:22:28 -0400

    made changes to kernel and main

--- main.cu
+++ main.cu
@@ -13,12 +13,13 @@ struct Drone {
 
 __global__ void flight(float x, float y, float dx, float dy, int n)
 {
-	x = rand() % 129;
-	y = rand() % 129;
+	int i = threadIdx.x;
+	
+	x[i] = rand() % 129;
+	y[i] = rand() % 129;
 	
 	//drone.dx = ((rand() % 600 + 30)/3600) * 0.5;	
-
-	int i = 0;	
+	
 	if(i < n)
 	{
 		drone.x += dx;
@@ -32,29 +33,43 @@ __global__ void flight(float x, float y, float dx, float dy, int n)
 			drone.y = drone.y * (-1);
 		}
 		printf("", drone.x, drone.y);
-
+		
 	}
 }
 
 
 int main (void)
 {
+	//init random numbers
 	srand(time(NULL));
-	
-	//initialize drone
-	struct Drone drone;
-	//init random value for dx and dy	
-	drone.dx = ((rand() % 600 + 30)/3600) * 0.5;
-	drone.dy = ((rand() % 600 + 30)/3600) * 0.5;
-		
-	
+	//set up host copies
+	float *x, *y, *dx, *dy;	
 	//set up device copies
 	float *d_x, *d_y, *d_dx, *d_dy;
 	int size = sizeof(float);
 
+	//allocate device memory
 	cudaMalloc((void **), &d_x, size);
 	cudaMalloc((void **), &d_y, size);
 	cudaMalloc((void **), &d_dx, size);
 	cudaMalloc((void **), &d_dy, size);
- 
+	
+	//allocate host memory
+	x = (float *)malloc(size);
+	y = (float *)malloc(size);
+ 	dx = (float *)malloc(size);
+	dy = (float *)malloc(size);
+	*dx = ((rand() % 601 + 30)/3600) * 0.5;
+	*dy = ((rand() % 601 + 30)/3600) * 0.5;
+
+	cudaMemcpy(d_dx, dx, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_dy, dy, size, cudaMemcpyHostToDevice);
+	
+	flight<<<1, N>>>(d_x, d_y, d_dx, d_dy, N);
+
+	free(x); free(y); free(dx); free(dy);
+
+	cudaFree(d_x); cudaFree(d_y); cudaFree(d_dx); cudaFree(d_dy);
+
+	return 0;
 }
