c50e0430db2cd3cc11aafe6bdb799f0782297fac
alfred316 <ashaker@kent.edu>
2016-10-17 12:17:21 -0400

    made changes to main, added cuda functions

--- main.cu
+++ main.cu
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 #include <math.h>
 
-#define DIM 512
+#define N 512
 
-struct drone {
+struct Drone {
 	float x;
 	float y;
 	float dx;
 	float dy;
 }
 
-__global__ void flight(struct drone, int n)
+__global__ void flight(float x, float y, float dx, float dy, int n)
 {
-	drone.x = rand() % 129;
-	drone.y = rand() % 129;
+	x = rand() % 129;
+	y = rand() % 129;
 	
 	//drone.dx = ((rand() % 600 + 30)/3600) * 0.5;	
 
 	int i = 0;	
 	if(i < n)
 	{
-		drone.dx = ((rand() % 600 + 30)/3600) * 0.5;
-		drone.dy = ((rand() % 600 + 30)/3600) * 0.5;
-		drone.x += drone.dx;
-		drone.y += drone.dy;
+		drone.x += dx;
+		drone.y += dy;
 		if(drone.x >= 128)
 		{
 			drone.x = drone.x * (-1);
@@ -42,5 +40,21 @@ __global__ void flight(struct drone, int n)
 int main (void)
 {
 	srand(time(NULL));
+	
+	//initialize drone
+	struct Drone drone;
+	//init random value for dx and dy	
+	drone.dx = ((rand() % 600 + 30)/3600) * 0.5;
+	drone.dy = ((rand() % 600 + 30)/3600) * 0.5;
+		
+	
+	//set up device copies
+	float *d_x, *d_y, *d_dx, *d_dy;
+	int size = sizeof(float);
 
+	cudaMalloc((void **), &d_x, size);
+	cudaMalloc((void **), &d_y, size);
+	cudaMalloc((void **), &d_dx, size);
+	cudaMalloc((void **), &d_dy, size);
+ 
 }
